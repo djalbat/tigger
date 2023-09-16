b0fb6118d7a1d84c19abe3f0e5ba07df2e2d7353
James Smith <james.smith@djalbat.com>
2023-09-16 13:01:11 +0100

    Exported the repository utilities.

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 
 const issueUtilities = require("./bin/utilities/issue"),
       commitUtilities = require("./bin/utilities/commit"),
-      commentUtilities = require("./bin/utilities/comment");
+      commentUtilities = require("./bin/utilities/comment"),
+      repositoryUtilities = require("./bin/utilities/repository");
 
 module.exports = {
   issueUtilities,
   commitUtilities,
-  commentUtilities
+  commentUtilities,
+  repositoryUtilities
 };
